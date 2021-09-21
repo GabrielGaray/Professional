5d336a149e03e4c56e447a9c75c85d2e85583245
gabyzenack <ggaray@holos.cl>
2021-09-21 19:30:20 -0300

    [RManager] Fix empty res and delete async RM

--- src/firebase/requestManager.js
+++ src/firebase/requestManager.js
@@ -2,11 +2,11 @@ import { getFirestore } from "./config";
 const db = getFirestore();
 
 class RequestManager {
-  async get(nameCollection, query) {
+  get(nameCollection, query) {
     let collection = db.collection(nameCollection);
     if (query) collection = collection.where(...query);
 
-    const res = await collection
+    return collection
       .get()
       .then((response) => {
         return parseRes(response.docs);
@@ -17,13 +17,11 @@ class RequestManager {
       .finally(() => {
         // setLoading(false);
       });
-
-    return res;
   }
 }
 
 const parseRes = (response) => {
-  if (response?.length < 2)
+  if (response?.length === 1)
     return { ...response[0].data(), id: response[0].id };
   return response.map((doc) => ({ ...doc.data(), id: doc.id }));
 };
